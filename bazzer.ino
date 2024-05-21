7d9f8d3e124d9481a2c14bf59cc4ab51303b17e0
Mohammed Vahith <114148409+Mohammedvahith@users.noreply.github.com>
2024-05-21 13:50:19 +0530

    Update bazzer.ino

--- bazzer.ino
+++ bazzer.ino
@@ -1,38 +1,37 @@
-int ultratrig=7;
-int ultraecho=6;
-int sound=3;
-int led=11;
+int ultratrig = 7;  // Ultrasonic sensor trigger pin
+ultraecho = 6;  // Ultrasonic sensor echo pin
+int sound = 3;      // Buzzer pin
+int led = 11;       // LED pin
+
 long duration;
 int distance;
+
 void setup() {
-  pinMode(11,OUTPUT);  //RED LIGHT
-  pinMode(7,OUTPUT);   // TRIGGER
-  pinMode(6,INPUT);    //ECHO
-  pinMode(3,OUTPUT);   //SOUND
+  pinMode(led, OUTPUT);       // Red light
+  pinMode(ultratrig, OUTPUT); // Trigger
+  pinMode(ultraecho, INPUT);  // Echo
+  pinMode(sound, OUTPUT);     // Sound
 }
 
 void loop() {
-  digitalWrite(11,0);
-  digitalWrite(ultratrig,0);
+  digitalWrite(led, LOW);      // Turn off LED initially
+  digitalWrite(ultratrig, LOW);
   delayMicroseconds(2);
-  digitalWrite(ultratrig,1);
+  digitalWrite(ultratrig, HIGH);
   delayMicroseconds(10);
-  digitalWrite(ultratrig,0);
-  duration=pulseIn(ultraecho,1);
-  distance=duration * 0.034 / 2;
-  if(distance >= 25 && distance <=30)
-  { 
-    digitalWrite(11,1);
-    digitalWrite(3,0);
-  }
-  else if(distance<15)
-  { 
-    digitalWrite(11,1);
-    digitalWrite(3,1);
-  }
-  else
-  {
-    digitalWrite(11,0);
-    digitalWrite(3,0);
+  digitalWrite(ultratrig, LOW);
+  
+  duration = pulseIn(ultraecho, HIGH);
+  distance = duration * 0.034 / 2;
+
+  if (distance >= 25 && distance <= 30) {
+    digitalWrite(led, HIGH);  // Turn on LED
+    digitalWrite(sound, LOW); // Turn off buzzer
+  } else if (distance < 15) {
+    digitalWrite(led, HIGH);  // Turn on LED
+    digitalWrite(sound, HIGH);// Turn on buzzer
+  } else {
+    digitalWrite(led, LOW);   // Turn off LED
+    digitalWrite(sound, LOW); // Turn off buzzer
   }
 }
