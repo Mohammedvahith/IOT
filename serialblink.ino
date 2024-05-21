a4d7984a282109436083837383c77142042a4b94
Mohammed Vahith <114148409+Mohammedvahith@users.noreply.github.com>
2024-05-21 13:48:27 +0530

    Update serialblink.ino

--- serialblink.ino
+++ serialblink.ino
@@ -1,12 +1,15 @@
+int ledPin = 8;      // LED pin
+
 void setup() {
-  pinMode(8,OUTPUT);
-  pinMode(2,OUTPUT);
+  pinMode(8,OUTPUT); // Initialize the LED pin as an output
+  pinMode(2,OUTPUT); // Initialize pin 2 as an output (not used in loop)
 }
+
 void loop() {
-  digitalWrite(8,1);
-  delay(200);
+  digitalWrite(8,1); // Turn the LED on
+  delay(200);        // Wait for 200 milliseconds
   
-  digitalWrite(8,0);
-   delay(100);
+  digitalWrite(8,0); // Turn the LED off
+   delay(100);       // Wait for 100 milliseconds
   
 }
