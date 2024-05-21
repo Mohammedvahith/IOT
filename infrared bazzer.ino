d73d6969def060644f99f9c697ede9ccd4581523
Mohammed Vahith <114148409+Mohammedvahith@users.noreply.github.com>
2024-05-21 13:44:24 +0530

    Update infrared bazzer.ino

--- infrared bazzer.ino
+++ infrared bazzer.ino
@@ -1,1 +1,31 @@
+const int IRout = 7;      // IR sensor output pin
+const int IRin = 6;       // IR sensor input pin
+const int buzzer = 3;     // Buzzer pin
 
+long duration;
+int distance;
+
+void setup() {
+  pinMode(IRout, OUTPUT);   // Infrared output
+  pinMode(IRin, INPUT);     // Infrared input
+  pinMode(buzzer, OUTPUT);  // Buzzer
+}
+
+void loop() {
+  digitalWrite(IRout, LOW);
+  delayMicroseconds(2);
+  digitalWrite(IRout, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(IRout, LOW);
+
+  duration = pulseIn(IRin, HIGH);
+  distance = duration * 0.034 / 2;
+
+  if (distance >= 25 && distance <= 30) {
+    digitalWrite(buzzer, LOW);  // Turn off buzzer
+  } else if (distance < 15) {
+    digitalWrite(buzzer, HIGH); // Turn on buzzer
+  } else {
+    digitalWrite(buzzer, LOW);  // Turn off buzzer
+  }
+}
