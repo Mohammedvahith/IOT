6b33f4f0dfd3d3669561ba6f150edd2c1a50619f
Mohammed Vahith <114148409+Mohammedvahith@users.noreply.github.com>
2024-05-23 01:02:58 +0530

    Update end_semester_lab.ino

--- end_semester_lab.ino
+++ end_semester_lab.ino
@@ -1,19 +1,22 @@
-const int bazzer = 13;
-const int irPin = 8;
+const int buzzerPin = 13; // Pin number for the buzzer
+const int irPin = 8;      // Pin number for the IR sensor
 
-void setup(){
-  Serial.begin(9600);
-  pinMode(bazzer, OUTPUT);
-  pinMode(irPin, INPUT); 
+void setup() {
+  Serial.begin(9600);         // Initializes serial communication with a baud rate of 9600 bits per second.
+  pinMode(buzzerPin, OUTPUT); // Initialize the buzzer pin as an output
+  pinMode(irPin, INPUT);      // Initialize the IR sensor pin as an input
 }
 
 void loop() {
-  int ir = digitalRead(irPin);
-  if (ir == LOW) {
-    digitalWrite(bazzer, HIGH);
-    }
-  else {
-    digitalWrite(bazzer, LOW);
-    }        
-
+  // Read the state of the IR sensor
+  int irState = digitalRead(irPin);
+  
+  // Check if the IR sensor detects an obstacle
+  if (irState == LOW) {
+    // Turn on the buzzer if obstacle detected
+    digitalWrite(buzzerPin, HIGH);
+  } else {
+    // Turn off the buzzer if no obstacle detected
+    digitalWrite(buzzerPin, LOW);
+  }        
 }
